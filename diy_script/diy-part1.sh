fbf539cac9e8c0bbb57264897aa5cd5cb927765b
ranqingwen <ranqingwen@gmail.com>
2026-04-03 09:46:12 +0800

    2026.4.3

---
+++ diy_script/diy-part1.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: 2410_x64_full_diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+#
+
+# 添加源仓库
+sed -i '/helloworld/d' feeds.conf.default
+sed -i '/small/d' feeds.conf.default
+sed -i '/passwall/d' feeds.conf.default
+#sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
+sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
+sed -i '$a src-git istore https://github.com/linkease/istore;main' feeds.conf.default
+sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
+sed -i '$a src-git passwall_packages https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git;main' feeds.conf.default
+sed -i '$a src-git passwall_luci https://github.com/Openwrt-Passwall/openwrt-passwall.git;main' feeds.conf.default
+
+# 添加 adguardHome
+#git clone --depth=1 --single-branch https://github.com/sirpdboy/luci-app-#adguardhome.git
+
+# 添加 argon 主题
+rm -rf feeds/luci/themes/luci-theme-argon
+rm -rf feeds/luci/applications/luci-app-argon-config
+git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
+
+# 添加 Lucky
+git clone https://github.com/gdy666/luci-app-lucky.git package/lucky
+
+# 添加 netdata
+#git clone https://github.com/sirpdboy/luci-app-netdata package/luci-app-netdata
+
+# 添加 oaf
+git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
+
+# 添加 openclaw
+#git clone https://github.com/10000ge10000/luci-app-openclaw.git package/luci-app-openclaw
+
+# 移除 openwrt feeds 自带的核心库
+rm -rf feeds/packages/net/{xray-core,v2ray-geodata,sing-box,chinadns-ng,dns2socks,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-libev,shadowsocks-rust,shadowsocksr-libev,simple-obfs,tcping,trojan-plus,tuic-client,v2ray-plugin,xray-plugin,geoview,shadow-tls}
+git clone https://github.com/xiaorouji/openwrt-passwall-packages package/passwall-packages
+
+# 添加 poweroffdevice
+git clone https://github.com/sirpdboy/luci-app-poweroffdevice.git package/luci-app-poweroffdevice
---
+++ diy_script/diy-part2.sh
@@ -0,0 +1,158 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: 2410_x64_full_diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
+
+echo "开始 DIY2 配置……"
+echo "========================="
+build_date=$(TZ=Asia/Shanghai date "+%Y.%m.%d")
+build_name="2410"
+
+# 修改主机名字，修改你喜欢的就行（不能纯数字或者使用中文）
+sed -i "/uci commit system/i\uci set system.@system[0].hostname='OpenWrt'" package/lean/default-settings/files/zzz-default-settings
+sed -i "s/hostname='.*'/hostname='OpenWrt'/g" ./package/base-files/files/bin/config_generate
+
+# 默认地址
+sed -i 's/192.168.1.1/192.168.23.250/g' package/base-files/files/bin/config_generate
+
+# 最大连接数修改为65535
+sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=65535' package/base-files/files/etc/sysctl.conf
+
+# 设置密码为空（安装固件时无需密码登陆，然后自己修改想要的密码）
+sed -i '/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF./d' package/lean/default-settings/files/zzz-default-settings
+
+# 调整 x86 型号只显示 CPU 型号
+sed -i 's/${g}.*/${a}${b}${c}${d}${e}${f}${hydrid}/g' package/lean/autocore/files/x86/autocore
+
+# 设置ttyd免帐号登录
+sed -i 's/\/bin\/login/\/bin\/login -f root/' feeds/packages/utils/ttyd/files/ttyd.config
+
+# 默认 shell 为 bash
+sed -i 's/\/bin\/ash/\/bin\/bash/g' package/base-files/files/etc/passwd
+
+# 设置argon为默认主题
+sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
+sed -i 's/Bootstrap theme/Argon theme/g' feeds/luci/collections/*/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/*/Makefile
+
+# 更改argon主题背景
+cp -f $GITHUB_WORKSPACE/personal/bg1.jpg package/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
+
+# 显示增加编译时间
+sed -i "s/DISTRIB_REVISION='R[0-9]\+\.[0-9]\+\.[0-9]\+'/DISTRIB_REVISION='@R$build_date'/g" package/lean/default-settings/files/zzz-default-settings
+sed -i "s/LEDE/OpenWrt_${build_name} by ranqw build/g" package/lean/default-settings/files/zzz-default-settings
+
+
+# 修改右下角脚本版本信息和登录页版本信息
+#cp -f $GITHUB_WORKSPACE/personal/argon/footer.ut package/luci-theme-argon/ucode/template/themes/argon/footer.ut
+#cp -f $GITHUB_WORKSPACE/personal/argon/footer_login.ut package/luci-theme-argon/ucode/template/themes/argon/footer_login.ut
+#sed -i "s/OpenWrt_2410_name by ranqw build @R build_date/OpenWrt_${build_name} by ranqw build @R${build_date}/g" \
+#package/luci-theme-argon/ucode/template/themes/argon/footer.ut \
+#package/luci-theme-argon/ucode/template/themes/argon/footer_login.ut
+
+# 修改右下角脚本版本信息
+sed -i 's|<a href="https://github.com/jerrykuku/luci-theme-argon" target="_blank">ArgonTheme <%# vPKG_VERSION %></a>|<a class="luci-link" href="https://github.com/ranqingwen/OpenWrt-autobuild/releases" target="_blank">下载最新版</a>|' package/luci-theme-argon/luasrc/view/themes/argon/footer.htm
+sed -i 's/<a class=\"luci-link\" href=\"https:\/\/github.com\/openwrt\/luci\" target=\"_blank\">Powered by <%= ver.luciname %> (<%= ver.luciversion %>)<\/a>/OpenWrt by ranqw/' package/luci-theme-argon/luasrc/view/themes/argon/footer.htm
+# 修改登录界面脚本版本信息
+sed -i "/<a class=\"luci-link\"/d; /<a href=\"https:\/\/github.com\/jerrykuku\/luci-theme-argon\"/d; s|<%= ver.distversion %>|OpenWrt/LEDE by ranqw @R$build_date|" package/luci-theme-argon/luasrc/view/themes/argon/footer_login.htm
+
+
+# 修改欢迎banner
+cp -f $GITHUB_WORKSPACE/personal/banner package/base-files/files/etc/banner
+
+# 修复 netdata 不会自动启动的问题
+echo ">>> Fix netdata init.d & enable service"
+if [ -f /etc/init.d/netdata ]; then
+  echo "netdata init script exists"
+else
+  if [ -f package/luci-app-netdata/root/etc/init.d/netdata ]; then
+    chmod +x package/luci-app-netdata/root/etc/init.d/netdata
+  fi
+fi
+mkdir -p package/base-files/files/etc/rc.d
+ln -sf ../init.d/netdata package/base-files/files/etc/rc.d/S99netdata
+mkdir -p package/base-files/files/etc/netdata
+cat << 'EOF' > package/base-files/files/etc/netdata/netdata.conf
+[global]
+    run as user = root
+    memory mode = ram
+[cloud]
+    enabled = no
+EOF
+mkdir -p package/base-files/files/etc/uci-defaults
+cat << 'EOF' > package/base-files/files/etc/uci-defaults/99-netdata
+#!/bin/sh
+if [ -x /etc/init.d/netdata ]; then
+  /etc/init.d/netdata enable
+  /etc/init.d/netdata restart
+fi
+exit 0
+EOF
+chmod +x package/base-files/files/etc/uci-defaults/99-netdata
+
+# 修复上游仓库不稳定造成ustream-ssl报错问题
+find . -type f \( -name "Makefile" -o -name "*.mk" \) -exec sed -i 's#https://git.openwrt.org/#https://github.com/openwrt/#g' {} \;
+if [ -f "$USTREAM_MK" ]; then
+  sed -i 's/^PKG_SOURCE_PROTO.*/PKG_SOURCE_PROTO:=git/' $USTREAM_MK
+  sed -i 's#https://github.com/openwrt/project/ustream-ssl.git#https://github.com/openwrt/ustream-ssl.git#g' $USTREAM_MK
+  sed -i 's#https://git.openwrt.org/project/ustream-ssl.git#https://github.com/openwrt/ustream-ssl.git#g' $USTREAM_MK
+  sed -i '/^PKG_SOURCE:=/d' $USTREAM_MK
+  sed -i '/^PKG_HASH:=/d'   $USTREAM_MK
+fi
+rm -rf dl/ustream-ssl-*
+rm -rf build_dir/target-*/ustream-ssl-*
+
+# 移除 default-settings 中的 UPnP
+find package/feeds -type f | xargs sed -i -e '/luci-app-upnp/d' -e '/luci-i18n-upnp/d' -e '/miniupnpd/d'
+sed -i '/luci-app-upnp/d' package/Makefile
+sed -i '/luci-i18n-upnp/d' package/Makefile
+sed -i '/miniupnpd/d' package/Makefile
+rm -f tmp/.package_install
+
+# 修复 default-settings 问题
+echo ">>> Purge default-settings (all variants)"
+find package/feeds -maxdepth 2 -type d -name "default-settings*" -exec rm -rf {} +
+rm -rf package/default-settings*
+
+mkdir -p package/base-files/files/etc/uci-defaults
+cat << 'EOF' > package/base-files/files/etc/uci-defaults/99-system
+#!/bin/sh
+uci set system.@system[0].hostname='OpenWrt'
+uci set system.@system[0].zonename='Asia/Shanghai'
+uci set system.@system[0].timezone='CST-8'
+uci -q delete system.ntp.server
+uci add_list system.ntp.server='ntp.aliyun.com'
+uci add_list system.ntp.server='time1.cloud.tencent.com'
+uci add_list system.ntp.server='time.apple.com'
+uci add_list system.ntp.server='time.windows.com'
+uci commit system
+exit 0
+EOF
+chmod +x package/base-files/files/etc/uci-defaults/99-system
+cat << 'EOF' > package/base-files/files/etc/uci-defaults/99-luci
+#!/bin/sh
+uci set luci.main.lang='zh_cn'
+uci commit luci
+exit 0
+EOF
+chmod +x package/base-files/files/etc/uci-defaults/99-luci
+
+find . -type f \( -name "Makefile" -o -name "*.mk" \) \
+-exec sed -i 's#https://git.openwrt.org/#https://github.com/openwrt/#g' {} \;
+
+rm -rf dl/ustream-ssl-* build_dir/target-*/ustream-ssl-*
+find package -type f | xargs sed -i \
+  -e '/luci-app-upnp/d' \
+  -e '/luci-i18n-upnp/d' \
+  -e '/miniupnpd/d' || true
+
+
+echo "========================="
+echo " DIY2 配置完成……"
