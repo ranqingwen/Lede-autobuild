2eb767b3fc602a1f2d1c80ace19cdb7da33bbde2
ranqingwen <ranqingwen@gmail.com>
2026-04-03 11:54:58 +0800

    更新 diy-part1.sh

--- diy_script/diy-part1.sh
+++ diy_script/diy-part1.sh
@@ -14,12 +14,13 @@
 sed -i '/helloworld/d' feeds.conf.default
 sed -i '/small/d' feeds.conf.default
 sed -i '/passwall/d' feeds.conf.default
-#sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
 sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
 sed -i '$a src-git istore https://github.com/linkease/istore;main' feeds.conf.default
-sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
-sed -i '$a src-git passwall_packages https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git;main' feeds.conf.default
-sed -i '$a src-git passwall_luci https://github.com/Openwrt-Passwall/openwrt-passwall.git;main' feeds.conf.default
+# sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
+# 核心依赖包 (PassWall2 仍需使用这个 packages 仓库获取核心工具)
+sed -i '$a src-git passwall_packages https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git;main' feeds.conf.defau
+# PassWall2 界面
+sed -i '$a src-git passwall2 https://github.com/Openwrt-Passwall/openwrt-passwall2.git;main' feeds.conf.default
 
 # 添加 adguardHome
 #git clone --depth=1 --single-branch https://github.com/sirpdboy/luci-app-#adguardhome.git
@@ -39,18 +40,16 @@ git clone https://github.com/gdy666/luci-app-lucky.git package/lucky
 # 添加 oaf
 git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
 
-# 添加 luci-app-istore (iStore 商店本体)
-git clone https://github.com/linkease/istore.git package/luci-app-istore
 
 # 添加 luci-app-quickstart (快速设置首页)
-git clone https://github.com/linkease/istore-quickstart.git package/luci-app-quickstart
+# git clone https://github.com/linkease/istore-quickstart.git package/luci-app-quickstart
 
 # 添加 openclaw
 #git clone https://github.com/10000ge10000/luci-app-openclaw.git package/luci-app-openclaw
 
 # 移除 openwrt feeds 自带的核心库
 rm -rf feeds/packages/net/{xray-core,v2ray-geodata,sing-box,chinadns-ng,dns2socks,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-libev,shadowsocks-rust,shadowsocksr-libev,simple-obfs,tcping,trojan-plus,tuic-client,v2ray-plugin,xray-plugin,geoview,shadow-tls}
-git clone https://github.com/xiaorouji/openwrt-passwall-packages package/passwall-packages
+# git clone https://github.com/xiaorouji/openwrt-passwall-packages package/passwall-packages
 
 # 添加 poweroffdevice
 git clone https://github.com/sirpdboy/luci-app-poweroffdevice.git package/luci-app-poweroffdevice
